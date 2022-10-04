446b4e4e9d3b7b2e53c8bf9bc0c39e96103f8c7a
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2022-10-04 11:48:45 +0530

    Update DivisibleBy3.cpp

--- DivisibleBy3.cpp
+++ DivisibleBy3.cpp
@@ -3,21 +3,16 @@ using namespace std;
 void solution()
 {
     int a, b;
+    //taking input for the numbers
     cin >> a >> b;
 
     int tempx = a % 3, tempy = b % 3;
     if (tempx == 0 || tempy == 0)
-    {
         cout << "0" << endl;
-    }
     else if (tempx == tempy)
-    {
         cout << "1" << endl;
-    }
-    else
-    {
+    else 
         cout << "2" << endl;
-    }
 }
 
 int main()
