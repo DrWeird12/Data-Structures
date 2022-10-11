2fde36bba61ad9c0517497a1457ca38f1b812e34
ratedHunter <91844702+akapoor30@users.noreply.github.com>
2022-10-11 21:06:53 +0530

    Create dynamicLocation.cpp

---
+++ dynamicLocation.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int sum(int *arr,int n){
+    int s=0;
+    for(int i=0;i<n;i++){
+        s+=arr[i];
+    }
+    return s;
+}
+
+
+int main(){
+
+int n;
+cin>>n;
+int *arr=new int[n];
+
+
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+
+cout<<"sum: "<<sum(arr,n)<<endl;
+
+return 0;
+}
