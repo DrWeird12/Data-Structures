f12571839edf0e752a58807ec14aa04c1c852909
rahi shukla <83490803+rahishukla@users.noreply.github.com>
2022-10-11 22:01:04 +0530

    Update MinNumberOfFlips.cpp

--- MinNumberOfFlips.cpp
+++ MinNumberOfFlips.cpp
@@ -1,12 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-void solution()
-{
+void solution(){
     int n;
     cin >> n;
     vector<int> vec(n);
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         cin >> vec[i];
     }
 
